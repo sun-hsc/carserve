789f3ff7726870226e615e505b740f41ddfc75dc
heshichen <1751833044@qq.com>
2021-03-12 21:26:10 +0800

    路径修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@
       <!--头部区域-->
       <el-header>
         <div>
-          <img src="../../assets/img/logo_car.png" alt class="img_logo" />
+          <img src="../assets/img/logo_car.png" alt class="img_logo" />
           <span>驾校预约管理系统</span>
         </div>
         <el-button type="info" @click="logout">退出</el-button>
